19e91614ca4e7588548809e58e4841a2344e7a56
sirhashir <umairhashir89@gmail.com>
2026-04-17 16:00:23 +0530

    Initial project setup with Spring Boot and Redis

---
+++ src/main/java/com/sirhashir/fluxlimiter/FluxLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.sirhashir.fluxlimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirhashir/fluxlimiter/FluxLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirhashir.fluxlimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
